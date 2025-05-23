4192851e2a282e3b2c77d0fc2ae2f7ec5914e14a
Jesús Hernández <jesushdez1303@gmail.com>
2025-05-23 19:19:53 -0400

    other changes

--- scripts/MainScript.js
+++ scripts/MainScript.js
@@ -155,25 +155,34 @@ load()
 // This section is favorite system
 
     favoriteBtn.addEventListener("click", ()=>{
-        const newOption = document.createElement("option");
+        const optionsCreated = document.getElementsByClassName("newOption");
+        let optionsValues = [];
+        for(let i = 0; i < optionsCreated.length; i++){
+            optionsValues.push(optionsCreated[i].value);
+        }
+        if(!(optionsValues.find(el => el === dataField.value))){
+        const newOption = document.createElement("div");
         const span = document.createElement("span");
         const divDelete = document.createElement("div");
         const deleteBtn = document.createElement("button");
 
+        deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24ffd" viewBox="0 0 40 40"><path fill="currentColor" d="M21.499 19.994L32.755 8.727a1.064 1.064 0 0 0-.001-1.502c-.398-.396-1.099-.398-1.501.002L20 18.494L8.743 7.224c-.4-.395-1.101-.393-1.499.002a1.05 1.05 0 0 0-.309.751c0 .284.11.55.309.747L18.5 19.993L7.245 31.263a1.064 1.064 0 0 0 .003 1.503c.193.191.466.301.748.301h.006c.283-.001.556-.112.745-.305L20 21.495l11.257 11.27c.199.198.465.308.747.308a1.06 1.06 0 0 0 1.061-1.061c0-.283-.11-.55-.31-.747z"/></svg>`
+
         span.textContent = dataField.value;
 
+        newOption.setAttribute("value", dataField.value)
         newOption.setAttribute("class", "newOption");
 
         divDelete.appendChild(deleteBtn);
         newOption.append(span, divDelete);
         favoriteContainer.appendChild(newOption);
-
+}
     });
 
     favoriteContainer.addEventListener("change", (e)=>{
-        console.log(e.target.value)
         dataField.value = e.target.value;
         searchButton.click();
+        favoriteContainer.value = "Seleccione una opción"
     });
 
 // falta agregar colores dinámicos, cambio de idioma, algunos retos adicionales...
